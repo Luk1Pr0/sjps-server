ddb092df531a58f1bd5d905373aafeff8e4cceb7
luk1pr0 <lpytel16@gmail.com>
2022-05-13 16:35:55 +0100

    Adding more comments to the routes to clarify code

--- routes/kontaktRoute.js
+++ routes/kontaktRoute.js
@@ -3,12 +3,20 @@ const router = require('express').Router();
 // IMPORTS
 const sendEmails = require('../sendEmail');
 
+// POST
 router.post('/', (req, res) => {
 	try {
+		// GET THE DATA FROM THE FORM
 		const { fullName, email, message } = req.body;
+
+		// RESPOND TO USER THAT FORM HAS BEEN RECEIVED
 		res.status(200).json('Form submitted');
+
+		// SEND EMAIL TO THE USER AND ADMIN
 		sendEmails(fullName, email, message);
+
 	} catch (error) {
+		// RESPOND WITH ERROR
 		res.status(400).json('An error occured');
 	}
 });
