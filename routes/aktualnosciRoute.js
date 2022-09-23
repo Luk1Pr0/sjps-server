1582d97010e88c3484dd44d32467de2a4c784c95
luk1pr0 <lpytel16@gmail.com>
2022-09-23 17:53:48 +0100

    Trying to fix new errors

--- routes/aktualnosciRoute.js
+++ routes/aktualnosciRoute.js
@@ -35,8 +35,25 @@ router.post('/', encodeFile, async (req, res) => {
 	}
 })
 
-// UPDATE EXISTING UPDATE
-router.put('/:id', encodeFile, async (req, res) => {
+// // UPDATE EXISTING UPDATE
+// router.put('/:id', encodeFile, async (req, res) => {
+
+// 	const uploadedFile = req.fileData;
+
+// 	try {
+// 		// UPDATE EXISITNG UPDATE IN THE DB
+// 		const existingUpdate = await UpdateModel.findOneAndUpdate({ _id: req.params.id }, { title: req.body.title, message: req.body.message, file: uploadedFile, fileUrl: '' });
+
+// 		// RETURN SUCCESSFULL RESPONSE
+// 		return res.status(200).json('Update updated')
+// 	} catch (error) {
+// 		// RETURN ERROR
+// 		return res.status(400).json('Cannot update existing update')
+// 	}
+// })
+
+// UPDATE EXISTING UPDATE (DUPLICATE OF THE ABOVE TESTING WITHOUT THE ENCODEFILE MIDDLEWARE)
+router.put('/:id', async (req, res) => {
 
 	const uploadedFile = req.fileData;
 
@@ -59,7 +76,7 @@ router.get('/', async (req, res, next) => {
 		const updatesFromDb = await UpdateModel.find();
 
 		// DECODE THE FILES FROM DATABASE FOR ALL UPDATES
-		const allUpdates = decodeFile(updatesFromDb, req);
+		// const allUpdates = decodeFile(updatesFromDb, req);
 
 		return res.status(200).json(allUpdates);
 
