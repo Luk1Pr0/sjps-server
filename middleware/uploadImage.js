856eb425a753c75913677d3a40b2802cd645203e
luk1pr0 <lpytel16@gmail.com>
2022-10-22 13:42:06 +0100

    Fixing s3 upload issue

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -8,12 +8,15 @@ const s3 = new AWS.S3({
 
 const uploadImageAndGetUrl = async (image) => {
 	try {
+		console.log('POSTING TO S3')
 		// UPLOAD THE FILE TO S3
 		const newImage = await s3.putObject({
 			Body: Buffer.from(image.data),
 			Bucket: process.env.AWS_BUCKET,
 			Key: image.name,
 		}).promise();
+
+		console.log('POSTED TO S3')
 
 		// GET THE URL OF THE UPLOADED IMAGE
 		const imageUrl = `https://s3-${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET}/${image.name}`
